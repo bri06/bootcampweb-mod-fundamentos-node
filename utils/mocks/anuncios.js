a5439c1c9d9bffc7db94e87428bcf56f21655150
Briam <briammartinez@gmail.com>
2019-01-23 19:55:28 +0100

    datos a insertar en la base de datos

---
+++ utils/mocks/anuncios.js
@@ -0,0 +1,46 @@
+const anunciosMocks = [
+  {
+    nombre: "Bicicleta",
+    venta: true,
+    precio: 230.15,
+    foto: "https://www.decathlon.es/media/850/8500543/big_4e36703b-01dd-4f6f-8bcf-f25b36fa80c2.jpg",
+    tags: [ "lifestyle", "motor"]
+  },
+  {
+    nombre: "iPhone 3GS",
+    venta: false,
+    precio: 50.00,
+    foto: "https://ss7.vzw.com/is/image/VerizonWireless/iphone6s-plus-gld-front?$device-lg$",
+    tags: [ "lifestyle", "mobile"]
+  },
+  {
+    nombre: "prueba 1",
+    venta: false,
+    precio: 50.00,
+    foto: "https://ss7.vzw.com/is/image/VerizonWireless/iphone6s-plus-gld-front?$device-lg$",
+    tags: [ "lifestyle", "mobile"]
+  },
+  {
+    nombre: "prueba 2",
+    venta: false,
+    precio: 50.00,
+    foto: "https://ss7.vzw.com/is/image/VerizonWireless/iphone6s-plus-gld-front?$device-lg$",
+    tags: [ "lifestyle", "mobile"]
+  },
+  {
+    nombre: "prueba 3",
+    venta: false,
+    precio: 50.00,
+    foto: "https://ss7.vzw.com/is/image/VerizonWireless/iphone6s-plus-gld-front?$device-lg$",
+    tags: [ "lifestyle", "mobile"]
+  },
+  {
+    nombre: "prueba 4",
+    venta: false,
+    precio: 50.00,
+    foto: "https://ss7.vzw.com/is/image/VerizonWireless/iphone6s-plus-gld-front?$device-lg$",
+    tags: [ "lifestyle", "mobile"]
+  }
+];
+
+module.exports = anunciosMocks;
