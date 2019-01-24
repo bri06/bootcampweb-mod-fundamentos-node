ae75eed610b9d4d535cd806e8f242620a39f7fb4
Briam <briammartinez@gmail.com>
2019-01-24 23:56:42 +0100

    llevada la logica de filtrado al modelo de agentes

--- models/Anuncio.js
+++ models/Anuncio.js
@@ -16,6 +16,28 @@ anuncioSchema.statics.listar = (filter, limit, skip, sort) => {
   return query.exec();
 }
 
+anuncioSchema.statics.filtrado = (nombre, venta, tag, precio) => {
+  const filter = {};
+  
+  if (nombre) filter.nombre = new RegExp('^' + nombre, "i");
+  if (venta) filter.venta = (venta.toLowerCase() == 'true'); 
+  if (tag) filter.tags = { $in: [tag] };
+  if (precio) {
+    const [gte, lte] = precio.split('-');
+    if (gte && lte) {
+      filter.precio = { $gte: gte, $lte: lte };
+    } else if (gte && !lte) {
+      filter.precio = { $gte: gte };
+    } else if (!gte && lte) {
+      filter.precio = { $lte: lte };
+    } else {
+      filter.precio = { $gte: gte };
+    }
+  }
+
+  return filter;
+}
+
 const Anuncio = mongoose.model('Anuncio', anuncioSchema);
 
 module.exports = Anuncio;
