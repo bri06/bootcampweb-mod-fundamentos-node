5c6edbb44eb8d6950bd23f8f4937775dde41ce00
Briam <briammartinez@gmail.com>
2019-01-24 19:29:45 +0100

    refactor de la carpeta servicios

--- services/anuncios.js
+++ services/anuncios.js
@@ -1,21 +1,49 @@
-// Los servicios van a llamar a la libreria que es la que nos devuelve los datos
-
-const anunciosMocks = require('../utils/mocks/anuncios');
+// const anunciosMocks = require('../utils/mocks/anuncios');
 const Anuncio = require('../models/Anuncio');
 
-function anuncioService() {
-  
-  return {
-    getAnuncios: function({tags}) {
-      // Anuncio.find({});
-      return Promise.resolve(Anuncio.find({}));
-    },
+module.exports.getAnuncios = async (req, res, next) => {
+  try {
+    const filter = {};
+    const { nombre, venta, tag, precio } = req.query;
 
-    createAnuncio: function({nombre}) {
-      Anuncio.save();
-      return Promise.resolve(anunciosMocks[0]);
+    if (nombre) filter.nombre = new RegExp('^' + req.query.nombre, "i");
+    if (venta) filter.venta = (venta.toLowerCase() == 'true'); //REVISAR OTRA VEZ
+    if (tag) filter.tags = { $in: [tag] };
+    if (precio) { //REFACTORIZAR
+      const [gte, lte] = precio.split('-');
+      if (gte && lte) {
+        filter.precio = { $gte: gte, $lte: lte };
+      } else if (gte && !lte) {
+        filter.precio = { $gte: gte };
+      } else if (!gte && lte) {
+        filter.precio = { $lte: lte };
+      } else {
+        filter.precio = { $gte: gte };
+      }
     }
+    const anuncios = await Anuncio.listar(filter);
+    res.status(200).json({
+      data: anuncios,
+      message: 'lista de anuncios'
+    });
+  } catch (err) {
+    next(err);
   }
-}
+};
 
-module.exports = anuncioService;
+module.exports.createAnuncio = (req, res, next) => {
+  const { body: data } = req;
+
+  let anuncio = new Anuncio(data);
+
+  anuncio.save((err, anuncio) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.status(201).json({
+      data: anuncio,
+      message: 'anuncio creado'
+    });
+
+  });
+};
