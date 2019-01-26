ae998269d877aa7fc9c3674578298f8a2317d93f
Briam <briammartinez@gmail.com>
2019-01-26 18:30:40 +0100

    refactor de getAnuncios con paginacion y primera version de listarTags

--- controller/anuncios.js
+++ controller/anuncios.js
@@ -2,32 +2,29 @@ const Anuncio = require('../models/Anuncio');
 
 module.exports.getAnuncios = async (req, res, next) => {
   try {
-    const filter = {};
-    const { nombre, venta, tag, precio } = req.query;
-    if (nombre) filter.nombre = new RegExp('^' + req.query.nombre, "i");
-    if (venta) filter.venta = (venta.toLowerCase() == 'true'); 
-    if (tag) filter.tags = { $in: [tag] };
-    if (precio) {
-      const [gte, lte] = precio.split('-');
-      if (gte && lte) {
-        filter.precio = { $gte: gte, $lte: lte };
-      } else if (gte && !lte) {
-        filter.precio = { $gte: gte };
-      } else if (!gte && lte) {
-        filter.precio = { $lte: lte };
-      } else {
-        filter.precio = { $gte: gte };
-      }
-    }
-    const anuncios = await Anuncio.listar(filter);
+    let { nombre, venta, tag, precio, skip, limit, fields, sort } = req.query;
+    const anuncioFiltrado = Anuncio.filtrado(nombre, venta, tag, precio);
+    const anuncios = await Anuncio.listar(anuncioFiltrado, skip, limit, fields, sort );
     res.status(200).json({
-      data: anuncios,
+      results: anuncios,
       message: 'Lista de anuncios'
     });
   } catch (err) {
     next(err);
   }
 };
+
+module.exports.listarTags = async (req, res, next) => {
+  try {
+    const listaTags = await Anuncio.find({}, {'tags': 1});
+    res.status(200).json({
+      results: listaTags,
+      message: 'Lista de tags'
+    });
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports.createAnuncio = async (req, res, next) => {
   try {
--- routes/api/anuncios.js
+++ routes/api/anuncios.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAnuncios, createAnuncio } = require('../../controller/anuncios');
+const { getAnuncios, createAnuncio, listarTags } = require('../../controller/anuncios');
 
-// Obtener todos los anuncios
 router.get('/', getAnuncios);
-
-// Guardar Anuncio
+router.get('/tags', listarTags);
 router.post('/save', createAnuncio);
 
 module.exports = router;
